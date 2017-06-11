171c821398c71644bb77c1e1e4569343ec7525ac
Jacob Zuo <chopinsky@live.com>
2017-06-10 20:03:04 -0500

    creating block header class

--- src/com/blockchain/encryption/BlockHeader.kt
+++ src/com/blockchain/encryption/BlockHeader.kt
@@ -1,5 +1,37 @@
 package com.blockchain.encryption
 
+import java.security.Timestamp
+
 /**
  * Created by Ziwei on 6/10/2017.
  */
+
+data class BlockHeader(val SignedBy: String, val TimeStamp: Timestamp, val BlockHash: String, val PreBlockHash: String, val RecordKey: ByteArray) {
+
+    @Override
+    fun equals(block: BlockHeader): Boolean {
+
+        if (SignedBy != block.SignedBy)
+            return false
+
+        if (TimeStamp != block.TimeStamp)
+            return false
+
+        if (BlockHash != block.BlockHash)
+            return false
+
+        if (PreBlockHash != block.PreBlockHash)
+            return false
+
+        if (RecordKey.size != block.RecordKey.size)
+            return false
+
+        for (i in 0..RecordKey.size) {
+            if (RecordKey[i] != block.RecordKey[i])
+                return false
+        }
+
+        return true
+    }
+
+}
